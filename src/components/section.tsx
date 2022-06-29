7f00fe0068abec2fff7a459c264f4082fb6e7498
Ole Magnus Johnsen <ole.magnus@me.com>
2022-06-29 19:37:08 +0200

    :lipstick: Section now has min height same as screen

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -4,7 +4,9 @@ interface Props {
 }
 
 const Section = ({ children, className }: Props) => (
-  <div className={`h-screen m-auto px-5 md:px-10 ${className ?? ""}`}>{children}</div>
+  <div className={`min-h-screen m-auto px-5 md:px-10 ${className ?? ""}`}>
+    {children}
+  </div>
 );
 
 export default Section;
