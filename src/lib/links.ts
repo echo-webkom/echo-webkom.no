1bcc25ca168c1a141dda6e9ec85661e4feec244b
Ole Magnus Fon Johnsen <me@omfj.no>
2023-12-07 23:43:01 +0100

    Update links

--- src/lib/links.ts
+++ src/lib/links.ts
@@ -7,7 +7,7 @@ export const links = [
   {
     title: "GitHub",
     desc: "Kildekode for echo sin nettside.",
-    to: "https://github.com/echo-webkom/echo-web",
+    to: "https://github.com/echo-webkom/echo-web-mono",
   },
   {
     title: "Instagram",
@@ -18,17 +18,12 @@ export const links = [
 
 export const newLinks = [
   {
-    title: "Beta nettside",
-    desc: "Den kommende nettsiden til echo. Fortsatt under utvikling.",
-    to: "https://beta.echo-webkom.no",
-  },
-  {
-    title: "Beta Sanity CMS",
+    title: "Sanity CMS",
     desc: "CMS for den kommende nettsiden til echo. Fortsatt under utvikling.",
     to: "https://cms.echo-webkom.no",
   },
   {
-    title: "Beta dokumentasjon",
+    title: "Dokumentasjon",
     desc: "Dokumentasjon for den kommende nettsiden til echo. Fortsatt under utvikling.",
     to: "https://docs.echo-webkom.no",
   },
