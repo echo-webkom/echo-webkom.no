c199c99c8a9519b459b1fbf51f708d95d5905a35
Ole Magnus Fon Johnsen <me@omfj.no>
2024-07-11 23:14:13 +0200

    🖼️ Add background image

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -8,8 +8,20 @@ module.exports = {
   semi: true,
   trailingComma: "all",
   tabWidth: 2,
-  plugins: ["@ianvs/prettier-plugin-sort-imports", "prettier-plugin-tailwindcss"],
+  plugins: [
+    "@ianvs/prettier-plugin-sort-imports",
+    "prettier-plguin-astro",
+    "prettier-plugin-tailwindcss",
+  ],
   importOrder: ["<THIRD_PARTY_MODULES>", "", "^@/(.*)$", "^[./]"],
   importOrderParserPlugins: ["typescript", "jsx"],
   importOrderTypeScriptVersion: "5.0.0",
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
 };
